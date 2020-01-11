c51bbd3f01815fa7aa354be0e1194da4036f1560
DanielPri <privorotskyd@gmail.com>
2020-01-11 17:19:31 -0500

    Chaser faces wind direction

--- Assets/Scripts/ChaserController.cs
+++ Assets/Scripts/ChaserController.cs
@@ -6,16 +6,45 @@ public class ChaserController : MonoBehaviour
 {
     [SerializeField] GameObject target;
     [SerializeField] float speed = 1f;
+    [SerializeField] Transform windDirection;
 
-    Vector2 displacement;
+    Vector2 facingDirection;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        setMovingDirection();
+    }
+
+    public void setMovingDirection()
+    {
+        facingDirection = windDirection.rotation * Vector2.right;
+        if(facingDirection.x > 0.2)
+        {
+            anim.SetTrigger("HORIZONTAL");
+            spriteRenderer.flipX = false;
+        }
+        else if(facingDirection.x < -0.2)
+        {
+            anim.SetTrigger("HORIZONTAL");
+            spriteRenderer.flipX = true;
+        }
+        else if(facingDirection.y < -0.2)
+        {
+            anim.SetTrigger("DOWN");
+        }
+        else if(facingDirection.y > 0.2)
+        {
+            anim.SetTrigger("UP");
+        }
     }
 }
