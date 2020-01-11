2573221df3402565ba58e5d8bfe4841f76c4f3ad
DanielPri <privorotskyd@gmail.com>
2020-01-11 14:47:08 -0500

    created chaser that follows gameObject

---
+++ Assets/Scripts/ChaserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserController : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] float speed = 1f;
+
+    Vector2 displacement;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
