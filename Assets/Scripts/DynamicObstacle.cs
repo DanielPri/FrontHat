70538af932818f22d1bef53d436b6b6f683ad2fb
DanielPri <privorotskyd@gmail.com>
2020-01-11 18:45:55 -0500

    Dynamic Objects can now be created

---
+++ Assets/Scripts/DynamicObstacle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicObstacle : MonoBehaviour
+{
+    [SerializeField] GameObject path;
+    [SerializeField] GameObject obstacle;
+    [SerializeField] float speed = 1f;
+
+    List<Transform> nodes;
+    Transform target;
+    int pathIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nodes = new List<Transform>();
+        foreach(Transform child in path.transform)
+        {
+            nodes.Add(child);
+        }
+        obstacle = Instantiate(obstacle, nodes[pathIndex].position, Quaternion.identity);
+        pathIndex++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pathIndex < nodes.Count)
+        {
+            target = nodes[pathIndex];
+            obstacle.transform.position = Vector2.MoveTowards(obstacle.transform.position, target.position, speed * Time.deltaTime);
+            if (Mathf.Approximately((obstacle.transform.position - target.position).magnitude, 0))
+            {
+                pathIndex++;
+            }
+        }
+    }
+}
