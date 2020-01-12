b8172cb0ea848f96e23ebf12ea84d3c08b65a083
Matt <matt.caccavelli@gmail.com>
2020-01-12 13:39:09 -0500

    added audio

---
+++ Assets/Scripts/AudioLooper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLooper : MonoBehaviour
+{
+
+    AudioSource audio;
+    public List<AudioClip> clips;
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = clips[Random.Range(0, clips.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(!audio.isPlaying)
+        {
+            audio.clip = clips[Random.Range(0, clips.Count)];
+            audio.Play();
+        }
+       
+    }
+}
