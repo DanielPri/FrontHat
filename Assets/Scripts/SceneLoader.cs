eee00edd99e1614ba20dc43977d296adfbbaae73
DanielPri <privorotskyd@gmail.com>
2020-01-12 06:08:30 -0500

    wait, no, this is the commit I thought the previous one was.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class SceneLoader : MonoBehaviour
+{
+    void OnEnable()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("ObstacleAndBGDev");
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+}
